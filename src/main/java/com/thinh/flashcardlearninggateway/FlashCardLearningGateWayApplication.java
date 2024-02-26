2950d1aaddbfdc64c6f4fc54f5c656eb581e2101
thinh <anvanthinh@gmail.com>
2024-02-26 23:48:10 +0700

    init gateway

---
+++ src/main/java/com/thinh/flashcardlearninggateway/FlashCardLearningGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.thinh.flashcardlearninggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCardLearningGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCardLearningGateWayApplication.class, args);
+	}
+
+}
