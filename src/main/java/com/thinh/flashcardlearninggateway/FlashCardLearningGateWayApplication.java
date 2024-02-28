3503a6cb812739721c246fa49438dd5d9fc2a0de
thinh <anvanthinh@gmail.com>
2024-02-28 17:45:31 +0700

    config to run gateway

--- src/main/java/com/thinh/flashcardlearninggateway/FlashCardLearningGateWayApplication.java
+++ src/main/java/com/thinh/flashcardlearninggateway/FlashCardLearningGateWayApplication.java
@@ -2,12 +2,13 @@ package com.thinh.flashcardlearninggateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FlashCardLearningGateWayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlashCardLearningGateWayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FlashCardLearningGateWayApplication.class, args);
+    }
 }
